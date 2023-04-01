d8068a521cf2ecd8f1cc0981649d49ba723ab881
Dan Yee <dyee3435@gmail.com>
2023-04-01 17:11:16 -0400

    add INSERT operation for "Documents" table

---
+++ resources/lambda/insert_document.py
@@ -0,0 +1,36 @@
+import os
+import json
+import boto3
+import psycopg2
+from aws_lambda_powertools.utilities import parameters
+
+def lambda_handler(event, context):   
+    SECRET = json.loads(parameters.get_secret(os.environ.get("DB_SECRET_NAME")))
+
+    connection = psycopg2.connect(
+        database=SECRET.get("engine"),
+        user=SECRET.get("username"),
+        password=SECRET.get("password"),
+        host=SECRET.get("host"),
+        port="5432",
+    )
+
+    try:
+        cursor = connection.cursor()
+
+        # insert new document
+        cursor.execute("INSERT INTO Documents (document_name, document_path, created_date, created_by) VALUES ('{}', '{}', '{}', '{}')".format(event.doc_name, event.doc_path, event.created_date, event.created_by))
+        connection.commit()
+        cursor.close()
+        connection.commit()
+
+        return {
+            'Access-Control-Allow-Origin': '*',
+            'statusCode': 200,
+            'body' : "Successfully Inserted New Document" 
+        }
+    except Exception as e:
+        return {
+            'statusCode': 400,
+            'body': str(e)
+        }
