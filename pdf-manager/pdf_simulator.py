707e6c52313d7add3ff62791c63412461013098a
Peter <63479070+peterb2396@users.noreply.github.com>
2023-04-18 19:38:19 -0400

    created pdf_manager for lambdas

--- pdf-manager/pdf_simulator.py
+++ pdf-manager/pdf_simulator.py
@@ -1,11 +1,11 @@
 from pypdf import PdfReader, PdfWriter
 from pdfStructure import pdfForm, pdfElement, Consts
-from pypdf.generic import BooleanObject, NameObject, IndirectObject, TextStringObject, NumberObject
+from pypdf.generic import NameObject,TextStringObject, NumberObject
 from pypdf.constants import *
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfform
 from reportlab.lib.colors import magenta, pink, blue, green
-import os, io
+import os, io, csv
 
 import xlsxwriter
 
@@ -56,6 +56,12 @@ class PdfGenerator():
         ht = 700
         visitedGroups = []
         for field in fields[:]:
+
+            # Start a new page and reset ht if we are near the bottom
+            if (ht <= 10): # less than 10px at bottom then start new page
+                c.showPage()
+                ht = 800 # reset our position to top of page
+                c.setFont("Courier", 14)
             
             if field.type == Consts.mcDisplay:
                 # Check if group has been visited yet. If not, add title
@@ -95,7 +101,101 @@ class PdfGenerator():
         # Handles populating rect and page height properties mostly
 
 
+    def generate_csv(form):
 
+        with open(form.name+'.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            
+            visitedGroups = [] # Used to keep track of radio groups so we only display once
+            fieldNames = [] # the list of names which we will display as the first row
+            
+            # Add the names of the fields to the top row as a header
+            for field in form.fields:
+                if (field.type == Consts.mcDisplay):
+                    group = field.choiceGroup
+
+                    if group not in visitedGroups:
+                        # Add this group field 
+                        fieldNames.append(group)
+                        visitedGroups.append(group)
+
+                else: # Not a multiple choice so just display its name
+                    fieldNames.append(field.name)
+        
+            # Write the names as a row
+            writer.writerow(fieldNames)
+
+            # Next add the data values for each response, first we include a space
+            writer.writerow([""])
+            
+            
+            for response in form.responses:
+                
+                # Each person gets their own row, an array of responses
+                answers = []
+                visitedGroups = []
+                
+                for field in response.fields:
+                    if (field.type == Consts.checkBoxDisplay):
+
+                        if (field.value in Consts.checkBoxYesState):
+                            value = Consts.checkBoxDisplayYes
+                        else:
+                            value = Consts.checkBoxDisplayNo
+
+
+                        answers.append(value) # Write the checkbox response in a readible fashion (yes/no)
+
+                    # MANAGE DISPLAY OF MULTIPLE CHOICE
+                    elif (field.type == Consts.mcDisplay):
+        
+                        group = field.choiceGroup
+
+                        if group not in visitedGroups:
+                            visitedGroups.append(group) # Avoid doing this group twice
+                            values = []
+                            for f in response.fields:
+                                # Iterate again over all fields to find all responses of this group
+                                if (f.type == Consts.mcDisplay):
+                                    
+                                    g = f.choiceGroup
+                                    v = f.choiceValue
+
+                                    # This is in our current target group
+                                    if (group == g):
+                                        # We responded yes to this field for this group
+                                        if (f.value in Consts.checkBoxYesState):
+                                            
+                                            values.append(v)
+                            # We have aquired all the response values for this option in values[].
+                            results = ""
+                            last_i = len(values) - 1
+                            if not values:
+                                results = "None"
+                            else:
+                                for i in range(len(values)):
+                                    if i == last_i:
+                                        results += values[i]
+                                    else:
+                                        # We dont use , here because that would denote a new cell
+                                        results += (values[i] + " & ")
+                            answers.append(results) # store our multiple choice selections as a cell for this column
+                            
+
+                        else: # Already did this group!
+                            continue
+
+
+                    # All other types just write its value! (text)
+                    # If its nothing, we denote NO RESPONSE?
+                    else:
+                        answers.append(field.value)
+                        
+                        # Note if no response it will append nothing and show as ,,,
+
+                # Done with this person, add a row of their answers
+                writer.writerow(answers)
+            file.close()
 
 
     # Generate excel from response
@@ -190,122 +290,6 @@ class PdfGenerator():
         workbook.close()
 
         
-    # NOTE: autonomous function for devin to take a response (json and source) and generate a pdf
-    # no other function calls, we use new helper function pdf_to_fields to get field objects that we can populate
-    # the field objects are useful for whether it was generated, what the type is, what the name is etc
-    def generate_pdf(response, source):
-
-        reader = PdfReader(source)
-        fields = PdfGenerator.pdf_to_fields(source) # unanswered - we need to fill in the answers from json data
-        # Fill in responses
-        # Response is request.body, json body from the call to the function
-        for key in response.keys():
-
-            # Fill in the field with the data:
-            # first get a reference to the current field, and define the response to this field from the input
-            field = fields[int(key)]
-            fieldValue = response[key]["value"] # Dictionary of field name : field value, looking for key "value"
-
-
-            # Field says firstname, so store the first name
-            if field.name in Consts.firstNameFields:
-                field.value = fieldValue.capitalize()
-
-            # Field says name so its probably first and last
-            elif field.name in Consts.nameFields:
-                splitName = fieldValue.split(" ")
-                
-                # Store first and maybe last name
-                firstName = splitName[0].capitalize()
-                if len(splitName) == 2:
-                    lastName = splitName[1].capitalize()
-                    field.value = firstName+" "+lastName
-                else: # Only have first name
-                    field.value = firstName
-
-                # We are given last name so store last name
-            elif field.name in Consts.lastNameFields:
-                field.value = fieldValue.capitalize()
-            
-            else: # Any non-name field need not be capitalized
-                field.value = fieldValue
-
-        writer = PdfWriter()
-        writer.append_pages_from_reader(reader)
-        writer.set_need_appearances_writer()
-        values = []
-
-        # For each field we now fix up the data for checkboxes converting to machine constants such as /Off
-        for r in fields[:]:
-
-            # If this is a checkbox or radio button, we must convert the "No" to "/Off", etc so pdf can understand.
-            if (r.type == Consts.checkBoxDisplay or r.type == Consts.mcDisplay):
-
-                if (r.type == Consts.mcDisplay):
-                    pass
-
-                if (r.value == Consts.checkBoxDisplayYes):
-                        if r.generated:
-                            r.value = Consts.checkBoxYesState[1]
-                        else: # use /0
-                            r.value = Consts.checkBoxYesState[0]
-                else:
-                        r.value = Consts.checkBoxNoState
-            # Add to our field dict for fields to be updates
-
-            values.append(r.value)
-
-
-        """(re purposed update_page_form_field_values)"""
-        k = 0
-        # On each page 
-        for page in writer.pages:
-            
-            # For each annotation (j is index of annot) on this page
-            for j in range(len( page[PageAttributes.ANNOTS] )):
-
-                writer_annot = page[PageAttributes.ANNOTS][j].get_object()  # type: ignore
-                # print(j, k)
-                
-                # Check if we should ignore this annotation, i.e. if its name is not one of our responses
-                skip = True
-                for r in fields:
-                    if (r.name == writer_annot.get(FieldDictionaryAttributes.T)):
-                        skip = False # Don't skip this one
-
-                if skip: # Skip this!
-                    continue #without incrimenting k
-
-               
-                # Update the value. Buttons get an extra value tag
-                if writer_annot.get(FieldDictionaryAttributes.FT) == "/Btn":
-                    writer_annot.update(
-                        {
-                            NameObject(
-                                AnnotationDictionaryAttributes.AS
-                            ): NameObject(values[k])
-                        }
-                    )
-                 
-                
-                writer_annot.update(
-                {
-                    NameObject(FieldDictionaryAttributes.V): 
-                    TextStringObject( values[k] )
-                }
-                )
-                # NOTE: Make read only did not fix invisible text issue!
-                # writer_annot.update({NameObject("/Ff"): NumberObject(1)})
-
-                k += 1 # Update total respect field index (index holds across pages)
-        
-        with open("output.pdf", "wb") as output_stream:
-            writer.write(output_stream)
-        output_stream.close()
-
-        #NOTE: Attempt to flatten did not fix invisible text. Try converting to image perhaps
-        # fillpdfs.flatten_pdf(newFile, 'flat.pdf', True)
-
     # This function will take an existing form response object
     # that has been return with it's responses by a client and create a new blank forum, then fill it in.
     # NOTE: This means we must store 'blank' copy as an attribute in the form object, which will server 3 purposes
@@ -313,6 +297,7 @@ class PdfGenerator():
         # 2) We can display a visual of the pdf to users on the app
         # 3) When the server recieves a response in object form, it can use the 'blank' to make the duplicate and fill in
 
+    # Should take a form RESPONSE
     def generatePdf(response, formFolder): #formFolder passed as 'path'
         # Get the form that this response associates to
         # org = Server.getOrgByID(response.orgID)
@@ -406,109 +391,6 @@ class PdfGenerator():
         #NOTE: Attempt to flatten did not fix invisible text. Try converting to image perhaps
         # fillpdfs.flatten_pdf(newFile, 'flat.pdf', True)
 
-
-
-    # pdf to fields - extracts all fields from document, anonymous (does not save anywhere just fires and forgets)
-    # NOTE: for devin, this is autonomous
-    def pdf_to_fields(path):
-
-        reader = PdfReader(path)
-        
-    
-        myFields = []
-        fieldIndex = 0
-        curFieldPage = 0
-
-        for page in reader.pages:
-            
-            if "/Annots" in page:
-                # Store the heights so we can flip the orientation on Textract coordinate mapping (to top-to-bottom)
-                
-
-                for annot in page["/Annots"]:
-      
-                    fieldData = annot.get_object()
-                    
-                    
-                    try:
-                            curFieldName = fieldData["/T"]
-                    except: # Key error /T - watermark?
-                        pass#print(fieldData)
-                    
-
-
-                    if (fieldData["/Subtype"] == "/Widget"):
-                        
-                        
-                        curFieldType = ""
-                        curFieldValue = ""
-                        curFieldIndex = fieldIndex
-                        curFieldRect = fieldData["/Rect"]
-                        curFieldGenerated = True
-                        curFieldPageHeight = page.mediabox.height
-                        curFieldPageWidth = page.mediabox.width
-                        try:
-                            curFieldName = fieldData["/T"]
-                        except: # Key error /T
-                            continue # Skip this bad data type
-                            #curFieldName = "Unsupported_"+str(fieldIndex)
-
-                        try:
-                            fieldTypeID = fieldData["/FT"]
-                        except: # Unsupported field type (MC?)
-                            # fieldTypeID = "UnsupportedType"
-                            continue # Skip this bad data type
-
-                        # Get whether the field came from a generated form by looking for '/TU'
-                        try:
-                            fieldData["/TU"]
-                        except:
-                            # If this fails it was not generated
-                            curFieldGenerated = False
-
-                        #print(curFieldName)
-
-                        # Handle check box metadata
-                        # Is it a check box or radio button
-                        if (fieldTypeID == Consts.checkTypeID):
-                            
-                            if (curFieldName.find(":") != -1):
-                                curFieldType = Consts.mcDisplay
-                            else: 
-                                 # Checkbox only code
-                                 curFieldType = Consts.checkBoxDisplay
-                            try:
-                                curFieldValue = fieldData["/V"]
-                            except:
-                                # Field is empty!
-                                curFieldValue = ""
-                            
-
-
-                            # Readable values to machine values
-                            # System created form has /V for yes to be /Yes, no to be /Off.
-                            # Adobe forms has yes to be /0, and no to be /On. So we fix this by making an array of Consts
-                            if (curFieldValue in Consts.checkBoxYesState):
-                                curFieldValue = Consts.checkBoxDisplayYes
-                            else:
-                                curFieldValue = Consts.checkBoxDisplayNo
-
-                        # Handle text box
-                        elif (fieldTypeID == Consts.textTypeID):
-                            curFieldType = Consts.textFieldDisplay
-                            try:
-                                curFieldValue = fieldData["/V"]
-                            except:
-                                # Field is empty!
-                                curFieldValue = ""
-
-                        # Append this field to our list
-                        curField = pdfElement(curFieldName, curFieldType, curFieldValue, curFieldIndex, curFieldRect, curFieldGenerated, curFieldPageHeight, curFieldPageWidth, curFieldPage)
-                        myFields.append(curField)
-                        fieldIndex = fieldIndex + 1
-            curFieldPage+=1
-
-        return myFields
     
     # This function will generate a new form object. It can be thought of as "Starting an Event", and members of the organization
     # are per say "Invited to the event". In this case, that means being sent a "pdfRequest" object - a request to fill in
