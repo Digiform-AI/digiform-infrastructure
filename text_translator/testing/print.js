08db1698e91829b40a3c2d1d07063f487e9c00e3
Prudhvi Raj <bprudhviraj77@gmail.com>
2023-05-06 16:12:15 -0400

    Print Screen UI

---
+++ text_translator/testing/print.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Printer from 'react-native-print';
+
+const PrintScreen = () => {
+  const [availablePrinters, setAvailablePrinters] = useState([]);
+
+  const handlePrintPress = async () => {
+    const printers = await Printer.selectPrinter();
+    setAvailablePrinters(printers);
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={handlePrintPress}>
+        <Text>Select Printer</Text>
+      </TouchableOpacity>
+      {availablePrinters.map(printer => (
+        <Text key={printer.name}>{printer.name}</Text>
+      ))}
+    </View>
+  );
+};
+
+export default PrintScreen;
