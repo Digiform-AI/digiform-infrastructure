768e0e38a6dbd565fa330037f75b2c8936661797
Prudhvi Raj Bhashampally <41112634+BhashampallyPrudhviRaj@users.noreply.github.com>
2023-05-07 03:00:01 +0530

    Delete tempCodeRunnerFile.py

--- text_translator/testing/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-if isChecked(file_path):
-    #     if any(char.isalpha() or char.isdigit() for char in data):
-    #     # print(data)
-    #         text_list.append(data)
-    #     else:
-    #         text_list.append("Yes\n")
-    # else:
-    
